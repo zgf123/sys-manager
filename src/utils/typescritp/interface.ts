ad6c74efbd3641a9f8de4c8cae7f50b534e9d82a
zhangguangfei <zhangguangfei>
2020-09-07 11:01:40 +0800

    feat: ts接口

--- src/utils/typescritp/interface.ts
+++ src/utils/typescritp/interface.ts
@@ -65,6 +65,118 @@ const mySearch: SearchFunc = (source, subString) => {
 }
 mySearch('a', 'b')
 
+/* --------------------------------------------------*{ 可索引的类型 }*-------------------------------------------------- */
 interface StringArray {
   [index: number]: string
 }
+
+const myArray: StringArray = ['a', 'b']
+const myStr: string = myArray[0]
+
+/* class Animal {
+  public name: string = 'a'
+}
+class Dog extends Animal {
+  private breed: string = 'b'
+}
+
+// 错误：使用数值型的字符串索引，有时会得到完全不同的Animal!
+interface NotOkay {
+  [x: number]: Animal
+  [x: string]: Dog
+} */
+
+interface NumberDictionary {
+  [index: string]: number
+  length: number // 可以，length是number类型
+  // name: string // 错误，`name`的类型与索引类型返回值的类型不匹配
+}
+
+/* --------------------------------------------------*{ 类类型 }*-------------------------------------------------- */
+/* interface ClockInterface {
+  currentTime: Date
+}
+
+class Clock implements ClockInterface {
+  public currentTime: Date = new Date()
+  constructor(h: number, m: number) {}
+  ddd: number = 1
+} */
+
+/* interface ClockConstructor {
+  new (hour: number, minute: number): ClockInterface
+}
+interface ClockInterface {
+  tick(): void
+}
+
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+  return new ctor(hour, minute)
+}
+
+class DigitalClock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+    console.log('beep beep')
+  }
+} */
+/* --------------------------------------------------*{ 继承接口 }*-------------------------------------------------- */
+
+/* interface Shape {
+  color: string
+}
+
+interface Square extends Shape {
+  sideLength: number
+}
+
+// let square = {} as Square
+let square = <Square>{}
+square.color = 'blue'
+square.sideLength = 10
+square.aa = 1 */
+
+/* --------------------------------------------------*{ 混合类型 }*-------------------------------------------------- */
+/* interface Counter {
+  (start: number): string
+  interval: number
+  reset(): void
+}
+
+function getCounter(): Counter {
+  let counter = <Counter>function(start: number) {}
+  counter.interval = 123
+  counter.reset = function() {}
+  return counter
+}
+
+let c = getCounter()
+c(10)
+c.reset()
+c.interval = 5.0 */
+
+/* --------------------------------------------------*{ 接口继承类 }*-------------------------------------------------- */
+
+/* class Control {
+  private state: any
+  select() {}
+}
+
+interface SelectableControl extends Control {
+  select(): void
+}
+
+class Button extends Control implements SelectableControl {
+  select() {}
+}
+
+class TextBox extends Control {
+  select() {}
+}
+
+// 错误：“Image”类型缺少“state”属性。
+class Image implements SelectableControl {
+  select() {}
+}
+
+class Location {} */
